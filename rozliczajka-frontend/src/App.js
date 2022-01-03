66b45a6561a3cb92c0ebe470ad68ed91457b621b
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-03 22:22:24 +0100

    Adds jumbotron

--- rozliczajka-frontend/src/App.js
+++ rozliczajka-frontend/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
 import './App.css';
 
 const App = () => {
 	return (
 		<div className='App'>
 			<Navbar />
+			<Jumbotron />
 		</div>
 	);
 };
