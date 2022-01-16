eb3fa162ac99d734cd980a2c42a444c793e6c246
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-16 20:01:19 +0100

    fix: button and input styling

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ label, onClick }) => {
   return (
     <button
-      className="px-8 py-4 bg-primary rounded-lg text-white text-lg"
+      className="px-8 py-4 bg-orange-500 hover:shadow-inner hover:scale-105 hover:bg-orange-600 transition rounded-lg text-white text-lg"
       onClick={onClick}
     >
       {label}
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -11,7 +11,7 @@ const Input = ({ label, name, type, placeholder, value, onChange }) => {
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="w-full focus:border-primary focus:ring-primary border border-gray-300 rounded-md"
+        className="w-full px-3 py-2 focus:border-primary focus:ring-primary border border-slate-300 rounded-md shadow-sm placeholder-slate-400 invalid:border-red-500 invalid:text-red-500 focus:invalid:border-red-500 focus:invalid:ring-red-500"
         required
       />
     </div>
