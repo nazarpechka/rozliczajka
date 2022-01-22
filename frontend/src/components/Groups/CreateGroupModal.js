2b5b81eb4fca0c2a833da196bd76c0cd35acd296
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-22 21:38:59 +0100

    fix: overlay modal

--- frontend/src/components/Groups/CreateGroupModal.js
+++ frontend/src/components/Groups/CreateGroupModal.js
@@ -37,7 +37,7 @@ const CreateGroupModal = ({ children, onCreation }) => {
     <>
       <span onClick={() => setHidden(false)}>{children}</span>
       {!hidden && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen bg-gray-900 bg-opacity-50 flex items-center justify-center">
+        <div className="fixed z-20 top-0 left-0 right-0 bottom-0 w-full h-screen bg-gray-900 bg-opacity-50 flex items-center justify-center">
           <div className="w-1/3 m-auto flex flex-col items-start bg-white rounded-md shadow-lg">
             <div className="w-full p-6 mb-2 border-b border-primary">
               <h2 className="text-4xl font-medium">Nowa grupa</h2>
