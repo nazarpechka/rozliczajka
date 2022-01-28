416466907713e97b756d2c2aebed1ef6ea49b671
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-28 18:24:14 +0100

    func: backend security and compression, .env file

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const compression = require("compression");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const errorHandler = require("./middleware/errorHandler");
-const setRoutes = require("./routes/index");
+const setRoutes = require("./routes");
 
 const router = express.Router();
 const app = express();
 
-const PORT = process.env.PORT || 4000;
-const MONGODB_URL =
-  "mongodb+srv://rozliczajka:Rozliczajka2022@cluster0.o33dn.mongodb.net/rozliczajka?retryWrites=true&w=majority";
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGODB_URL);
+mongoose.connect(MONGO_URL);
 
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api", setRoutes(router));
