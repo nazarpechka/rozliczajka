c3fb9dc2702ce44a170e4d4d9829b9ba70a23afc
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-01-30 14:38:33 +0100

    fix: heroku deployment

--- backend/config/app.js
+++ backend/config/app.js
@@ -29,10 +29,10 @@ app.use("/api", errorHandler);
 
 // Heroku deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build/")));
+  app.use(express.static(path.join(__dirname, "../../frontend/build/")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/build/", "index.html"));
   });
 }
 
